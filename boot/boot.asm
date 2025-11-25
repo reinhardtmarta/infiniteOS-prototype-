c9365a7b7c3797c142e2a169aedac24f08aa706c
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-11-25 17:43:55 -0300

    Refactor bootloader entry point and global label

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,16 +1,10 @@
-; boot/boot.asm – bootloader 32-bit direto (o mais simples e infalível do mundo)
-; nasm -f elf32 boot/boot.asm -o boot/boot.o
-
-global start
+bits 32
 extern kernel_main
 
-bits 32
-
 section .text
-align 4
-
-start:
-    mov esp, 0x90000          ; stack em 576KB
-    call kernel_main          ; pula pro C
+global _start
+_start:
+    mov esp, 0x90000
+    call kernel_main
     cli
     hlt
