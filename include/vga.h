a63f4c8f4fee8fd113706d1c706371221f3c1afa
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-11-25 19:03:27 -0300

    Refactor VGA header guard and clean up includes

--- include/vga.h
+++ include/vga.h
@@ -1,17 +1,8 @@
-// include/vga.h
-#ifndef _VGA_H
-#define _VGA_H
-
-#include <stdint.h>
-
-#define VGA_WIDTH  80
-#define VGA_HEIGHT 25
-#define VGA_BUFFER 0xB8000
+#ifndef VGA_H
+#define VGA_H
 
 void vga_clear(void);
-void vga_print(const char *str);
-void vga_print_hex(uint32_t n);
 void vga_putchar(char c);
-void vga_set_color(uint8_t fg, uint8_t bg);
+void vga_print(const char* s);
 
 #endif
