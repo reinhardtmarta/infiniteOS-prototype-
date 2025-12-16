93a880b52da5f13a23538adfbcfded69b371a8a4
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-16 18:30:10 -0300

    Update kernel_main.c

--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -1,19 +1,14 @@
 #include "../drivers/vga/vga.h"
 #include "../drivers/timer/timer.h"
+
+#include "kernel.h"
 #include "idt.h"
 #include "paging.h"
 #include "scheduler.h"
-#include "kernel.h"
 
-void kernel_main(void) {
-    kernel_init();
-
-#ifdef CI_BUILD
-    __asm__("hlt");
-#else
-    while (1) __asm__("hlt");
-#endif
-}
+/* -------------------------------------------------
+ * Tasks de teste (scheduler)
+ * ------------------------------------------------- */
 
 void task_a(void) {
     vga_write("A ");
@@ -23,20 +18,31 @@ void task_b(void) {
     vga_write("B ");
 }
 
+/* -------------------------------------------------
+ * Kernel entry point
+ * ------------------------------------------------- */
+
 void kernel_main(void) {
     vga_write("InfiniteOS booting\n");
 
+    /* Inicialização base */
     idt_init();
     timer_init();
     paging_init();
 
 #ifdef CI_BUILD
     vga_write("CI build OK\n");
-    __asm__("hlt");
+    __asm__ volatile ("hlt");
 #else
+    /* Scheduler só roda fora do CI */
     scheduler_init();
     scheduler_add(task_a);
     scheduler_add(task_b);
     scheduler_run();
 #endif
+
+    /* Segurança: nunca retornar */
+    for (;;) {
+        __asm__ volatile ("hlt");
+    }
 }
