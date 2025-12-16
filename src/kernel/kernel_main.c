01d54f9bfec6acc30bef465ac7e88eae32c5c150
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-16 13:57:56 -0300

    Update kernel_main.c

--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -1,12 +1,9 @@
-// kernel_main.c — Kernel mínimo para InfiniteOS
-// Compila em freestanding mode (sem libc)
+#include "../drivers/vga/vga.h"
 
-#include <stdint.h>
+void kernel_main(void) {
+    vga_write("InfiniteOS booted.\n");
 
-void kernel_main() {
-    // Por enquanto apenas trava a CPU em hlt.
-    // Depois você pode chamar drivers, printar texto, inicializar memória, etc.
     while (1) {
-        __asm__ __volatile__("hlt");
+        __asm__("hlt");
     }
 }
