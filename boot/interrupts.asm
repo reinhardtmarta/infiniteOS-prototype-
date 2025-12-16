a8c0b9e7337e9b568fdf375e7895f066c5eafeed
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-16 14:08:03 -0300

    Update interrupts.asm

--- boot/interrupts.asm
+++ boot/interrupts.asm
@@ -1,18 +1,9 @@
-; ====================== boot/interrupts.asm ======================
-
-; Certifique-se de que a função idt_load é global
-global idt_load
-
-; Localize o ponteiro e adicione global:
-global idt_load_ptr ; <--- ADICIONE ISSO!
-
-idt_load_ptr:
-    dw 0x0000 ; Limite (tamanho da IDT - 1)
-    dd 0x00000000 ; Endereço base da IDT
 bits 32
 global isr_timer
+extern timer_tick
 
 isr_timer:
     pusha
+    call timer_tick
     popa
     iret
