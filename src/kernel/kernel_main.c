58def69264bd7922d64921d62d696fd4145d3de8
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-16 14:05:44 -0300

    Update kernel_main.c

--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -1,7 +1,12 @@
 #include "../drivers/vga/vga.h"
+#include "../drivers/timer/timer.h"
+#include "idt.h"
 
 void kernel_main(void) {
-    vga_write("InfiniteOS booted.\n");
+    vga_write("InfiniteOS alive\n");
+    idt_init();
+    timer_init();
+    vga_write("Timer armed\n");
 
     while (1) {
         __asm__("hlt");
