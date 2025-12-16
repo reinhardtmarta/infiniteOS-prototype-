0bf09580f904349017fc3142eb519765b5c68975
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-16 18:32:25 -0300

    Create scheduler.h

---
+++ include/scheduler.h
@@ -0,0 +1,12 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <stdint.h>
+
+typedef void (*task_fn)(void);
+
+void scheduler_init(void);
+void scheduler_add(task_fn fn);
+void scheduler_tick(void);
+
+#endif
