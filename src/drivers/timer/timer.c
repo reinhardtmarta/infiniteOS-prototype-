c501eeb43274f01b65e7778bbf2bd9f29e6f1aec
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-16 14:09:50 -0300

    Update timer.c

--- src/drivers/timer/timer.c
+++ src/drivers/timer/timer.c
@@ -1,7 +1,22 @@
 #include "timer.h"
+#include "../vga/vga.h"
 
-static inline void outb(unsigned short port, unsigned char val) {
+static volatile uint32_t ticks = 0;
+
+static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+void timer_tick(void) {
+    ticks++;
+
+    if (ticks % 100 == 0) {
+        vga_write(".");
+    }
+}
+
+uint32_t timer_get_ticks(void) {
+    return ticks;
 }
 
 void timer_init(void) {
