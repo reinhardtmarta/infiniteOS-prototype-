a3386e39974b5609b23b7fba5e053772d9adf9ae
Marta Reinhardt <martinhareinhardt@gmail.com>
2025-12-16 14:22:54 -0300

    Update kernel_main.c

--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -2,17 +2,26 @@
 #include "../drivers/timer/timer.h"
 #include "idt.h"
 #include "paging.h"
+#include "scheduler.h"
+
+void task_a(void) {
+    vga_write("A");
+}
+
+void task_b(void) {
+    vga_write("B");
+}
 
 void kernel_main(void) {
-    vga_write("InfiniteOS\n");
+    vga_write("InfiniteOS scheduler online\n");
 
     idt_init();
     timer_init();
     paging_init();
 
-    vga_write("Paging enabled\n");
+    scheduler_init();
+    scheduler_add(task_a);
+    scheduler_add(task_b);
 
-    while (1) {
-        __asm__("hlt");
-    }
+    scheduler_run();
 }
